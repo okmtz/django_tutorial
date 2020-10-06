67f8455d262ede9a14254ae1e332d814efaa8349
souken-b <yuto430scuderia@gmail.com>
2020-10-06 11:40:14 +0900

    viewの作成

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # djangoがマッチする正規表現を見つけるとdjangoは指定されたビュー関数を呼び出す。
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    # コンテキストはテンプレート変数名をPythonオブジェクトにマッピングする辞書
+    return render(request, 'polls/index.html', context)
 # Create your views here.
-    
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question'; question})
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on questin %s." % question_id)
