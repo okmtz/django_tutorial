c10f2540d0bae4ef3e7bf12e17051e498e9aac0d
souken-b <yuto430scuderia@gmail.com>
2020-10-06 12:57:45 +0900

    テストの作成

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name ~ 'latest_question_list'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
